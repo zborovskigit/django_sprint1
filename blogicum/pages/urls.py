000e585497a4ecc11f329a049dac34c4368fd544
zborovski <me@zborovski.ru>
2024-05-06 11:53:22 +0300

    Fixed problem with flake8 tests

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -7,6 +7,8 @@ from django.urls import path
 app_name = 'pages'
 
 urlpatterns = [
-    path('about/', TemplateView.as_view(template_name='pages/about.html'), name='about'),
-    path('rules/', TemplateView.as_view(template_name='pages/rules.html'), name='rules'),
+    path('about/', TemplateView.as_view(
+         template_name='pages/about.html'), name='about'),
+    path('rules/', TemplateView.as_view(
+         template_name='pages/rules.html'), name='rules'),
 ]
